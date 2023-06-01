b9160f682be928a25db834e4021374fcc9ee22ba
Obaid <obaid@gmail.com>
2023-06-01 15:11:55 +0530

    addi

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="">
+        <h1>General Store</h1>
+      </div>
+      <div>
+        <ul>
+          <li>Home </li>
+          <li>Products</li>
+          <li>About</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Header;

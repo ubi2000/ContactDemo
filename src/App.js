25c449461799fab79d63a856bd5dd71c1559674c
Obaid <obaid@gmail.com>
2023-06-01 23:47:00 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Products from "./components/Products";
 
 function App() {
   return (
-    <div>
+    <div style={{backgroundColor: "black", borderRadius:"30px",height:"100vh"}}>
       <Header />
+      <Products />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import "./Header.css";
+import ShopLogo from "../images/shoplog.png";
+import Logo from "../images/cartlogo.png";
+import Icon from "../images/icon.png";
 
 const Header = () => {
   return (
-    <div>
-      <div className="">
-        <h1>General Store</h1>
+    <div className="header">
+      <div className="left-logo">
+        <img src={ShopLogo} alt="alt"></img>
+        <h2>General Store</h2>
       </div>
-      <div>
-        <ul>
+      <nav className="navbar">
+        <ul className="items">
           <li>Home </li>
           <li>Products</li>
           <li>About</li>
+        </ul>
+      </nav>
+      <div className="cart-div">
+        <ul>
+          <img src={Logo} alt="alt"></img>
+
+          <button>Login</button>
+          <img src={Icon} alt="alt" />
         </ul>
       </div>
     </div>
